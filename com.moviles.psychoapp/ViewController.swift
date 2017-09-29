cee52074b549537a34ff0d1031df478d7abf134a
Gustavo Adolfo Alegria Zu±iga <ga.alegria10@isc332.local>
2017-09-29 10:59:21 -0500

    Initial Commit

---
+++ com.moviles.psychoapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  com.moviles.psychoapp
+//
+//  Created by Gustavo Adolfo Alegria Zu±iga on 29/09/17.
+//  Copyright © 2017 Gustavo Adolfo Alegria Zu±iga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
